1e57c617adf7d181df403c2c8969ca956bbb536b
okapippo <96384731+okapippo@users.noreply.github.com>
2022-02-28 15:48:07 +0900

    Add files via upload

--- ArgsLastPrinter.java
+++ ArgsLastPrinter.java
@@ -1,12 +1,17 @@
  public class ArgsLastPrinter {
    public static void main(String[] args) {
+     for (int i = 0; i < args.length; i++) {
+       System.out.println(args[i]);
+     }
 
-     if (args.length == 0) {
+     if (args.length == 0){
        System.out.println("なし");
      }
-
-     else {
+     else{
        System.out.println(args[args.length]);
      }
-   }
+
+
+
+    }
  }
